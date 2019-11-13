488baf66b6c750e65259bdc18e961903bf7a493c
takuzouakg <0126dandy@gmail.com>
2019-11-13 06:59:48 +0000

    add admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'admins/index'
+  get 'admins/create'
+  get 'admins/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/admins_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class AdminsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get admins_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get admins_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get admins_new_url
+    assert_response :success
+  end
+
+end
