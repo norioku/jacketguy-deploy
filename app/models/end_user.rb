dfab8b7558d419c94bc6a9d5d135e61085b6389c
Mashimo Hiroki <mc.mdt1007it@gmail.com>
2019-11-14 03:59:21 +0000

    Set up Admin

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,7 +3,7 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # acts_as_paranoid
+  acts_as_paranoid
 
   validates :last_name, presence: true
   validates :first_name,  presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
 
-  get 'admins/:id' => 'admisn#index', as: 'admin'
+  # get 'admins/:id' => 'admins#index', as: 'admin'
   get 'admins/create'
   get 'admins/new'
+  get 'admins/index' =>'admins#index'
   get 'users/:id' => "users#show", as: 'user'
 
   devise_for :end_users, controllers: {
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
+  registrations: 'users/registrations'
   # registrations: 'admins/registrations'
 }
 
