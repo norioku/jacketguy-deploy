34a761e81bb8b50ef0f5a951966d20124ccd4f5e
takuzouakg <0126dandy@gmail.com>
2019-11-16 07:35:05 +0000

    add view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,19 @@
 class ProductsController < ApplicationController
 
 def index
+	 @all_ranks = Product.find(User_favorite.group(:product_id)
+	 							.order('count(product_id) descs')
+	 							.limit(6) .pluck(:product_id))
+	 @product = Product.find(params[:id])
 end
 
 def show
+	 @product = Product.find(params[:id])
+	 @all_reviews = @product.reviews
 end
 
 def search
+	
 end
 
 end
