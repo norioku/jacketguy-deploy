13ba9e9c0e16aa2eaeb79854f7a6ed37552520ed
yuki-koda <pix7al3@yahoo.co.jp>
2019-11-17 02:59:30 +0000

    fix header & footer

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -3,7 +3,7 @@ class UserReviewsController < ApplicationController
 def index
   # @products = Products.all
   @user = EndUser.find(params[:id])
-  debugger
+
 end
 
 end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -2,7 +2,7 @@ class EndUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable,  :validatable
 
   validates :last_name,           presence: true
   validates :first_name,          presence: true
