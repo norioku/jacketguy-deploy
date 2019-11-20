6095b423b9eae54e1482cd1769730ab033bcb3da
yuki-koda <pix7al3@yahoo.co.jp>
2019-11-20 04:52:21 +0000

    add admin_review edit

--- app/controllers/user_order_histories_controller.rb
+++ app/controllers/user_order_histories_controller.rb
@@ -1,9 +1,8 @@
 class UserOrderHistoriesController < ApplicationController
 
 def show
-  @user = EndUser.find(params[:id])
-  @order_histories = OrderHistory.where(end_user_id:@user)
   @order_history = OrderHistory.find(params[:id])
+  @order_histories = OrderHistory.where(end_user_id: @order_history.end_user.id)
 end
 
 end
