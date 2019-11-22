607bc509f340b5f899d93fc8592fe3c79b110aa9
takuzouakg <0126dandy@gmail.com>
2019-11-22 03:12:48 +0000

    add user productus

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -6,4 +6,20 @@ class UserReviewsController < ApplicationController
     @reviews = Review.where(product_id: @product)
   end
 
+  def create
+  	product = Product.find(params[:id])
+  	review = Review.new(review_params)
+  	review.product_id = product.id
+  	review.end_user_id = current_end_user.id
+  	review.save
+  	redirect_to product_path(product.id)
+  end
+
+
+private
+
+def review_params
+	params.require(:review).permit(:review_content)
+end
+
 end
