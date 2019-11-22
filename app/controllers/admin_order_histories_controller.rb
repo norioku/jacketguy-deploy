4ba37f4b260198af98a9921fc94d6ab25262707e
yuki-koda <pix7al3@yahoo.co.jp>
2019-11-22 03:26:29 +0000

    fjajaj

--- app/controllers/admin_order_histories_controller.rb
+++ app/controllers/admin_order_histories_controller.rb
@@ -6,33 +6,17 @@ end
 
 def show
   @order_history = OrderHistory.find(params[:id])
-  @product_histories = ProductHistory.where(order_history_id: @order_history)
-
-  # @order_histories.each do |order_history|
-  #   order_history.product_histories.each do |product_history|
-  #     @product_history.subtotal = product_history.product.price * product_history.order_quantity
-  #   end
-  # end
-
-  # @product_history.subtotal.save
-
-
-  # binding.pry
-  # @product_history.subtotal = @product_histories.order_quantity * @product.price
-  # @order_history.product_total_price = @product.price * @product_histories.order_quantity
-
-  # productのpriceとproduct_historyのorder_quantityを掛ける→product_historyのsubtotal
-  # productのsubtotalを商品の数分足して、さらにshipping_feeを足す→order_historyのproduct_total_price
+  @product_histories = ProductHistory.where(order_history_id: @order_history).order(:product_id).page(params[:page]).per(5)
 end
 
 def update
   @order_history = OrderHistory.find(params[:id])
 
   if @order_history.update(admin_order_history_params)
-    flash[:notice] = "発送ステータスを更新しました"
+    flash[:success] = "発送ステータスを更新しました"
     redirect_to admins_order_histories_path
   else
-    flash[:notice] = "発送ステータスの更新に失敗しました"
+    flash[:danger] = "発送ステータスの更新に失敗しました"
     render :show
   end
 end
--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -3,7 +3,7 @@ class UserReviewsController < ApplicationController
   def index
     @user = current_end_user
     @product = Product.find(params[:id])
-    @reviews = Review.where(product_id: @product)
+    @reviews = Review.where(product_id: @product).order(:id).page(params[:page]).per(5)
   end
 
 end
