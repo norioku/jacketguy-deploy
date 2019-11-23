7490fc9d24ae80d4921d0e66f444b1e4f62fdf72
Mashimo Hiroki <mc.mdt1007it@gmail.com>
2019-11-23 12:45:22 +0000

    Succeed Ajax

--- app/controllers/user_favorites_controller.rb
+++ app/controllers/user_favorites_controller.rb
@@ -4,14 +4,16 @@ class UserFavoritesController < ApplicationController
 		@product = Product.find(params[:id])
 	            favorite = current_end_user.favorites.new(product_id: @product.id)
 	            favorite.save
-	            redirect_to product_path(@product)
+
 	end
 
 	def destroy
 		@product = Product.find(params[:id])
 	            favorite = current_end_user.favorites.find_by(product_id: @product.id)
 	            favorite.destroy
-	            redirect_to product_path(@product)
+
 	end
 
 end
+
+
