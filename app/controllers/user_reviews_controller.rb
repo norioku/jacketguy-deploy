e157faacf79abeef64ed42e0d4e737fff95395f1
takuzouakg <0126dandy@gmail.com>
2019-11-25 11:40:11 +0000

    finish review

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -8,15 +8,17 @@ class UserReviewsController < ApplicationController
 
   def create
     product = Product.find(params[:id])
-    unless Review.where(end_user_id: params[:end_user_id], product_id: params[:product_id]).empty?
+    if Review.exists?(end_user_id: current_end_user, product_id: product.id)
+       flash[:danger] = "レビューが投稿できません"
+       redirect_to product_path(product.id)
+    else
       review = Review.new(review_params)
+      review.end_user_id = current_end_user.id
       review.product_id = product.id
       review.save
       flash[:success] = "レビューを投稿しました"
       redirect_to product_path(product.id)
-    else
-      flash[:danger] = "レビューが投稿できません"
-      redirect_to product_path(product.id)
+
     end
 
   end
@@ -24,7 +26,7 @@ class UserReviewsController < ApplicationController
 private
 
 def review_params
-	params.require(:review).permit(:review_content)
+	params.require(:review).permit(:review_content,:end_user_id,:product_id)
 end
 
 end
