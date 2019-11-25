6a687af0a8ed895f63498f8ef5b17147bd6a16fa
takuzouakg <0126dandy@gmail.com>
2019-11-25 05:57:23 +0000

    limit review

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
 
   def show
      @product = Product.find(params[:id])
-     @reviews = @product.reviews
+     @reviews = @product.reviews.limit(5)
      @user = current_end_user
      @review = Review.new
      @arrival_stocks = @product.arrival_records.all.sum(:arrival_product)
